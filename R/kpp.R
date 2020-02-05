7b18fde4f5f30ebbcd87bced02d3dfcfcd050b0a
jacquesattack <jacques.rossignol@gmail.com>
2020-02-05 09:29:52 -0800

    Initial commit.

---
+++ R/kpp.R
@@ -0,0 +1,47 @@
+#' Calculate Euclidean distance between two vectors
+#'
+#' @param v1 First vector
+#' @param v2 Second vector
+#' @return The Euclidean distance
+dist = function(v1,v2) {
+  sqrt(sum((v1-v2)^2))
+}
+
+#' Calculate initial centers for K-means clustering using the K-means++ algorithm
+#'
+#' @param data An n x 2 data.frame
+#' @param k The desired number of centers to return
+#' @return A data.frame of centers
+#' @export
+#' @examples
+#' d = faithful %>% rename(x = eruptions, y = waiting)
+#' kpp_centers = kpp(d,2)
+#' kmeans(d,centers = kpp_centers)
+kpp = function(data,k) {
+  centroids = numeric(k)
+
+  # assign first centroid at random from points
+  centroids[1] = sample(1:nrow(data),1)
+
+  for(i in 2:k) {
+    # For each data point x, compute D(x), the distance between
+    # x and the nearest center that has already been chosen
+    d_x = apply(data,1,function(row){
+      # find closest center
+      d_to_centers = sapply(centroids[1:(i-1)],
+                            function(center) {
+                              dist(row,data[center,])
+                            })
+      min(d_to_centers)
+    })
+
+    # Choose one new data point at random as a new center,
+    # using a weighted probability distribution where a point x
+    # is chosen with probability proportional to D(x)^2.
+    probs = d_x^2 / sum(d_x)
+    zero_prob = which(d_x == 0)
+    centroids[i] = sample(seq_along(probs)[-zero_prob],1,prob=probs[-zero_prob])
+  }
+
+  return(data[centroids,])
+}
